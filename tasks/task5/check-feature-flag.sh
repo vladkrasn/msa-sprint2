2a95655f0a4dd6826e69afc816f1a743fab4f281
Vladimir Krasnochub <vkrasnochub@saminfotech.com>
2025-08-06 22:56:55 +0400

    Task 5

--- tasks/task5/check-feature-flag.sh
+++ tasks/task5/check-feature-flag.sh
@@ -5,4 +5,4 @@ set -e
 echo "▶️ Проверка Feature Flag (X-Feature-Enabled: true)..."
 
 # Отправляем запрос с заголовком, чтобы маршрутизировать трафик на `v2`
-curl -H "X-Feature-Enabled: true" http://localhost:9090/ping
+curl -H "X-Feature-Enabled: true" http://localhost:63437/ping
